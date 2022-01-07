8fa2262ffbe266c6fda5fe851e84597afd04491f
tshiamo <tslephal>
2022-01-07 23:42:44 +0200

    Refactoring code

--- src/main/java/za/co/wethinkcode/game_of_life/http/responses/WorldResponse.java
+++ src/main/java/za/co/wethinkcode/game_of_life/http/responses/WorldResponse.java
@@ -4,13 +4,14 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 
 public class WorldResponse {
     private final int id;
-    private final int epoch = 0;
+    private final int epoch;
     private final int[][] state;
 
     @JsonCreator
     public WorldResponse(final int worldId, final int[][] state) {
         this.id = worldId;
         this.state = state;
+        this.epoch = 0;
     }
 
     public int getId() {
